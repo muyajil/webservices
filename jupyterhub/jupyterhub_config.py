ea8d8735355ab4153cbb869020fafbc5657dedf7
Mohammed Ajil <mohammed@ajil.ch>
2020-05-15 13:44:50 +0200

    added bitwarden and jupyterhub

---
+++ jupyterhub/jupyterhub_config.py
@@ -0,0 +1,18 @@
+# ln this file to /home/srv-user/volumes/jupyterhub/jupyterhub_config.py
+import os
+
+c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+c.Spawner.default_url = '/lab'
+c.JupyterHub.services = [
+    {
+        'name': 'cull_idle',
+        'admin': True,
+        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+    },
+]
+c.Authenticator.whitelist = {'muy', 'chrigi', 'vince', 'dmaag', 'fdaniel'}
+c.Authenticator.admin_users = {'muy'}
+c.SystemUserSpawner.host_homedir_format_string = '/home/srv-user/volumes/jupyterhub/home/{username}'
