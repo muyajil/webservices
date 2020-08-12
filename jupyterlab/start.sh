36a5685884327a1aef39fc45f5b1363fcf263ae8
Mohammed Ajil <mohammed@ajil.ch>
2020-08-12 14:05:59 +0200

    enable sudo possibility AND install sudo

--- jupyterlab/start.sh
+++ jupyterlab/start.sh
@@ -87,13 +87,13 @@ if [ $(id -u) == 0 ] ; then
     fi
 
     # Enable sudo if requested
-    #if [[ "$GRANT_SUDO" == "1" || "$GRANT_SUDO" == 'yes' ]]; then
-    #    echo "Granting $NB_USER sudo access and appending $CONDA_DIR/bin to sudo PATH"
-    #    echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
-    #fi
+    if [[ "$GRANT_SUDO" == "1" || "$GRANT_SUDO" == 'yes' ]]; then
+       echo "Granting $NB_USER sudo access and appending $CONDA_DIR/bin to sudo PATH"
+       echo "$NB_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
+    fi
 
     # Add $CONDA_DIR/bin to sudo secure_path
-    #sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
+    sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
 
     # Exec the command as NB_USER with the PATH and the rest of
     # the environment preserved
