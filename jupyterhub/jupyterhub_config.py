a345d53664848ec38db4f213f1f00641afed5920
Mohammed Ajil <mohammed@ajil.ch>
2020-08-12 13:41:31 +0200

    try to replicate base-notebook

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -3,6 +3,7 @@ import os
 c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.DockerSpawner.remove = True
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
 c.Spawner.default_url = '/lab'
 c.JupyterHub.services = [
--- jupyterlab/init.sh
+++ jupyterlab/init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 mkdir -p ~/envs
-if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
-    echo "Set $NB_USER UID to: $NB_UID"
-    usermod -u $NB_UID $NB_USER
-fi
-jupyterhub-singleuser
+. /usr/local/bin/start-notebook.sh
